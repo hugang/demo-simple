df630b31e78d988a974172bb2fce7c13cd1c61e0
hugang <hugang0825@gmail.com>
2023-11-09 17:01:48 +0900

    update

---
+++ demo-simple-server/src/main/java/io/hugang/demo/DemoSimpleApplication.java
@@ -0,0 +1,11 @@
+package io.hugang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSimpleApplication.class, args);
+    }
+}
---
+++ demo-simple-ui/src/router/index.ts
@@ -0,0 +1,54 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+export const menuRoutes: RouteRecordRaw[] = [
+	{
+		path: '/p/simple',
+		meta: {
+			title: 'Demo Simple',
+			icon: 'icon-appstore'
+		},
+		children: [
+			{
+				path: '/simple/api',
+				name: 'api',
+				component: () => import('../views/api/index.vue'),
+				meta: {
+					title: 'api list',
+					icon: 'icon-fire'
+				}
+			}
+		]
+	}
+]
+
+export const constantRoutes: RouteRecordRaw[] = [
+	{
+		path: '/redirect',
+		component: () => import('../layout/index.vue'),
+		children: [
+			{
+				path: '/redirect/:path(.*)',
+				component: () => import('../layout/components/Router/Redirect.vue')
+			}
+		]
+	},
+	{
+		path: '/',
+		component: () => import('../layout/index.vue'),
+		redirect: '/simple/api',
+		children: [...menuRoutes]
+	},
+	{
+		path: '/404',
+		component: () => import('../views/404.vue')
+	},
+	{
+		path: '/:pathMatch(.*)',
+		redirect: '/404'
+	}
+]
+
+export const router = createRouter({
+	history: createWebHashHistory(),
+	routes: constantRoutes
+})
